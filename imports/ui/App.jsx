bd9eebdeb72db24b1e72eefdf718a64b3c832a1f
Cezar Mocan <mocancezar@gmail.com>
2018-09-08 22:47:36 -0400

    Style updates

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,6 +5,7 @@ import { createContainer } from 'meteor/react-meteor-data';
 import Message from './Message.jsx';
 import {MessageQueue} from '../api/MessageQueue.js';
 import {ServerTime} from '../api/ServerTime.js';
+import About from './About.jsx'
 
 import moment from 'moment'
 
@@ -12,14 +13,25 @@ import moment from 'moment'
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      aboutOpen: false
+    }
     const handle = Meteor.subscribe('MessageQueue')
     const handle2 = Meteor.subscribe('ServerTime')
+    this.onClick = this.onClick.bind(this)
   }
 
   componentWillReceiveProps(newProps) {
   }
 
   componentDidMount() {
+  }
+
+  onClick() {
+    console.log('clicked about')
+    this.setState({ 
+      aboutOpen: !this.state.aboutOpen
+    })
   }
 
   render() {
@@ -34,20 +46,21 @@ class App extends Component {
       serverTime = moment.unix(serverTimestamp).format('MMMM Do YYYY, h:mm:ss a');
     }
 
+    const { aboutOpen } = this.state
     //[August 22, 2015, 14:13:22]
 
     return (
       <div className="container">
-        <div className="about-container">
-        </div>
         <div className="message-container">
           <span className="purple tiny">[{serverTime}]</span>
           <Message currMessages={messages}/>
         </div>
         <div className="footer">
           <span className="purple">New Haven, CT | </span> <span className="orange">Princeton, NJ</span>
-          <br/>
-          <span className="orange">about</span>
+        </div>
+        <About onShow={aboutOpen}/>
+        <div className="about-link-container">
+          <span className="link" onClick={this.onClick}>{aboutOpen ? 'close' : 'about'}</span>
         </div>
       </div>
     );
